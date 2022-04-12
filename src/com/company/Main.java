77de97cb3847ca31804eeaf027f8bb5487b7b672
Kristan Frost <kristan.frost@gmail.com>
2022-04-12 12:44:28 -0500

    Change arrays demo a bit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,13 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] someInts = new int[10];
-        System.out.println(someInts); // this isn't very useful
-        System.out.println(Arrays.toString(someInts)); //that's better
+        System.out.println(someInts);
+//        System.out.println(Arrays.toString(someInts));
 
         // if you don't declare the values of each element in the Array it defaults them all to 0
 
@@ -18,17 +17,6 @@ public class Main {
         int[] someOtherInts = {1,3,5,7,9,11,13}; // we didn't have to declare the size, it gets dictated by the values
 //        System.out.println(Arrays.toString(someOtherInts));
 
-        // Arrays are much stricter in Java. There size is fixed, and they can only hold one type
-        // How rude.
-
-        // What else has Java got?
-//        ArrayList<Integer> myList = new ArrayList<Integer>();
-//        System.out.println(myList);
-//        myList.add(1);
-//        myList.add(15);
-//        myList.add(42);
-
-        // ArrayLists are more similar to what we are used to from JS, but they still only support one type of data.
     }
 
 }
